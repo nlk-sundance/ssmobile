11ebec108c7c8a3c4f04864fcd2cb082039c5a47
AmitDeo <magpie.arise@gmail.com>
2016-01-01 11:45:33 +0545

    Header Banner

--- iphone/front-page.php
+++ iphone/front-page.php
@@ -3,13 +3,12 @@
 
 	<?php wptouch_the_post(); ?>
 	<div class="<?php wptouch_post_classes(); ?> page-title-area rounded-corners-8px dlform">
-
 		<h2 role="heading">Locate a Dealer</h2>
-	<form action="#" method="get" id="dltopform"<?php if (isset($_GET['dlzip'])) echo ' class="zipposted"'; ?>>
-	<div id="dlbtn"></div>
-	<input type="text" data-role="none" id="dlzip" name="dlzip" value="<?php echo (isset($_GET['dlzip']) ? $_GET['dlzip'] : 'Zip/Postal Code'); ?>" onblur="if(this.value=='') this.value='Zip/Postal Code';" onfocus="if(this.value=='Zip/Postal Code') this.value='';" />
-	<input type="submit" data-role="none" value="SEARCH" class="dlsub" />
-	</form>
+		<form action="#" method="get" id="dltopform"<?php if (isset($_GET['dlzip'])) echo ' class="zipposted"'; ?>>
+		<div id="dlbtn"></div>
+		<input type="text" data-role="none" id="dlzip" name="dlzip" value="<?php echo (isset($_GET['dlzip']) ? $_GET['dlzip'] : 'Zip/Postal Code'); ?>" onblur="if(this.value=='') this.value='Zip/Postal Code';" onfocus="if(this.value=='Zip/Postal Code') this.value='';" />
+		<input type="submit" data-role="none" value="SEARCH" class="dlsub" />
+		</form>
 	</div>
 	<div id="dlandimg"></div>
     <div id="dlrap"><div id="dlresult"></div></div>
